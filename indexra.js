146e04ba9051fc00af5543b72c46bf0ba4dd4e53
Artem <134812175+artem1984A@users.noreply.github.com>
2024-10-13 15:54:24 +0200

    Update indexra.js

--- indexra.js
+++ indexra.js
@@ -558,70 +558,7 @@ app.get('/welcome', (req, res) => {
 
 
 
-app.post('/submitmesx', async (req, res) => {
-  try {
-    // Extract name, email, and message from the form data
-    const { name, email, message } = req.body;
 
-    // Check if any of the fields are empty
-    if (!name && !email && !message) {
-      console.log("No valid data provided, skipping processing.");
-      return res.status(400).send('No valid data provided.');
-    }
-
-    // Instantiate the Message class with the form data
-    const userMessage = new Message(name, email, message);
-
-    // Use the formattedMessage method to convert the properties into a string
-    const formattedMessage = userMessage.formattedMessage();
-
-    const TOKEN = "da95d54c576369870c39b46f762ba713";
-    const ENDPOINT = "https://send.api.mailtrap.io/";
-
-    const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
-
-    const sender = {
-      email: "mailtrap@arnhem-front-end.site",
-      name: "ryzhov",
-    };
-
-    const recipients = [
-      { email: "torbezpeka.bayer.post@gmail.com" }
-    ];
-
-    // Send the primary email if there is a valid message
-    if (message) {
-      await client.send({
-        from: sender,
-        to: recipients,
-        subject: "Hy from arnhem-front-end.site!",
-        text: formattedMessage,
-        category: "Integration Test",
-      });
-
-      console.log("Email sent successfully");
-    }
-
-    // Send the reply email only if the email is present and valid
-    if (email) {
-      await userMessage.sendReply();
-      console.log("Reply email sent successfully");
-    }
-
-    // Write the message to a file only if the message text is present
-    if (message) {
-      const child = userMessage.writeToFile('user_message.txt');
-      userMessage.catchErrorsMessage(child, res); // Handle child process errors
-    } else {
-      // If no file writing is necessary, still send the success response
-      res.redirect('/profile.html');
-    }
-
-  } catch (error) {
-    console.error("Error processing request:", error);
-    res.status(500).send('Error processing your request');
-  }
-});
 
 app.set('views', path.join(__dirname, 'views')); // Set the directory for views
 app.set('view engine', 'ejs'); // Set EJS as the view engine
